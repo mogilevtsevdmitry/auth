f762e8e3d48e3cdd3475baa7680a8b201a3bab1a
mogilevtsevdmitry <webmogilevtsev@ya.ru>
2023-06-18 20:57:12 +0500

    .

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,17 +1,9 @@
-import { PrismaExceptionFilter } from '@common/filters';
 import { Global, Module } from '@nestjs/common';
-import { APP_FILTER } from '@nestjs/core';
 import { PrismaService } from './prisma.service';
 
 @Global()
 @Module({
-    providers: [
-        PrismaService,
-        {
-            provide: APP_FILTER,
-            useClass: PrismaExceptionFilter,
-        },
-    ],
+    providers: [PrismaService],
     exports: [PrismaService],
 })
 export class PrismaModule {}
